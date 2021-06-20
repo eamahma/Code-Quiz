8c6a6b63da8b615cbb9c0cd94f44c186a26aa997
eamahma <eamahma@gmail.com>
2021-06-20 13:44:07 -0400

    question and answer array created

--- js/script.js
+++ js/script.js
@@ -25,6 +25,29 @@
 // 4. square brackets
 //
 
+// color variables
+// --bgdLight = #7e307e;
+// --bgdLDark = #800080;
+
+var questions = [{
+    question: "Commonly used data types DO NOT include",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: 2
+}, {
+    question: "Arrays in JavaScript can be used to store _____.",
+    choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    answer: 3
+}, {
+    question: "A Very useful tool used during development and debugging for printing content to debugger is:",
+    choices: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log"],
+    answer: 3
+}, {
+    question: "The Condition in an if / else statement is enclosed in _____.",
+    choices: ["1. qoutes", "2. curly brackets", "3. parantheses", "4. square brackets"],
+    answer: 2
+}];
+
+// define body of document
 var body = document.body;
 // use h1 to show questions
 var h1El = document.createElement("h1");
@@ -58,13 +81,25 @@ listEl.appendChild(li4);
 // set attributes
 h1El.setAttribute("style", "margin: auto; width: 50%; text-align: left;");
 Answers.setAttribute("style", "margin: auto; width: 50%; text-align: left; font-size: 20px");
-li1.setAttribute("style", "marging: auto; padding: 0.2em; background: purple;");
-li2.setAttribute("style", "marging: auto; padding: 0.2em; background: purple;");
+li1.setAttribute("style", "marging: auto; padding: 0.2em; background-color: #800080;");
+li2.setAttribute("style", "marging: auto; padding: 0.2em; background-color: #800080;");
 li3.setAttribute("style", "marging: auto; padding: 0.2em; background: purple;");
 li4.setAttribute("style", "marging: auto; padding: 0.2em; background: purple;");
 
 
 
+// // mouse over list items
+// li1.addEventListener("mouseover", event =>{
+//     // li1.setAttribute("style", "background-color: var(--bgdLight);");
+//     li1.setAttribute("style", "background-color: #7e307e;");
+//     console.log("on list1");
+// });
+
+// li1.addEventListener("mouseout", event =>{
+//     // li1.setAttribute("style", "background-color: var(--bgdLight);");
+//     li1.setAttribute("style", "background-color: #800080;");
+//     console.log("out list1");
+// });
 
 
 
