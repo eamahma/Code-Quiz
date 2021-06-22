5b4a378fd89790cb9cb28465ccda7aa84ab0da71
eamahma <eamahma@gmail.com>
2021-06-22 19:10:17 -0400

    refactored the code and some bugs cleared

--- js/script.js
+++ js/script.js
@@ -37,11 +37,12 @@ var wrongFlag = false;
 var page = 0;
 // variable to define quiz time
 var timeLeft = 40;
+var timePause =2;
 // an arry of initials
 var initials = [];
 // high scores Array
 var high_scores =[];
-
+var scoreListLength = 0;
 // define body of document
 var body = document.body;
 // define nav bar
@@ -82,8 +83,18 @@ var addInitial = document.createElement("div");
 var initialLabel = document.createElement("span");
 var initialInput = document.createElement("input");
 var initialButton = document.createElement("span");
-var initialList = document.createElement("ul");
+var initialList = document.createElement("ol");
 var initialListItem = document.createElement("li");
+
+// variables in High Score screen for clear and return buttons
+var scoreButtons = document.createElement("div");
+var clearButton = document.createElement("span");
+var returnButton = document.createElement("span");
+
+// var for dynamic list for scores 
+var scoreSection = document.createElement("div");
+var scorelistEl = document.createElement("ol");
+
 
 // creating elements
 body.appendChild(navBar);
@@ -117,6 +128,12 @@ body.appendChild(addInitial);
 addInitial.appendChild(initialLabel);
 addInitial.appendChild(initialInput);
 addInitial.appendChild(initialButton);
+
+// high score page buttons
+body.appendChild(scoreSection);
+body.appendChild(scoreButtons);
+scoreButtons.appendChild(returnButton);
+scoreButtons.appendChild(clearButton);
 
 
 // set attributes
@@ -153,10 +170,11 @@ addInitial.setAttribute("style", "display: none;");
 initialButton.setAttribute("style", "display: none;");  
 initialButton.setAttribute("class", "button");
 
+
 // mouse event hover over answers to change back ground volor
 span1.addEventListener("mouseover", event =>{
     span1.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em;");
-    console.log("Span1");
+    // console.log("Span1");
 });
 
 span1.addEventListener("mouseout", event =>{
@@ -211,8 +229,10 @@ span1.addEventListener("click", event => {
         timeLeft = timeLeft - 10;
         wrongFlag = true;
     }
+    
     result.setAttribute("style", "display: on; color: gray; font-style: italic; margin: auto; width: 50%; text-align: left; font-size: 20px");
-    answeredFlag = true;
+
+    stopTimer();
 });
 
 span2.addEventListener("click", event => {
@@ -228,6 +248,7 @@ span2.addEventListener("click", event => {
         timeLeft = timeLeft - 10;
     }
     result.setAttribute("style", "display: on; color: gray; font-style: italic; margin: auto; width: 50%; text-align: left; font-size: 20px");
+    stopTimer();
 });
 
 span3.addEventListener("click", event => {
@@ -244,6 +265,7 @@ span3.addEventListener("click", event => {
     }
     result.setAttribute("style", "display: on; color: gray; font-style: italic; margin: auto; width: 50%; text-align: left; font-size: 20px");
     answeredFlag = true;
+    stopTimer();
 });
 
 span4.addEventListener("click", event => {
@@ -260,17 +282,24 @@ span4.addEventListener("click", event => {
     }
     result.setAttribute("style", "display: on; color: gray; font-style: italic; margin: auto; width: 50%; text-align: left; font-size: 20px");
     answeredFlag = true;
+    stopTimer();
 });
 
 spanStart.addEventListener("click", event => {
     page = 1;
     Answers.setAttribute("style", "display: none;");
     spanStart.setAttribute("style", "display: none;");
+    scoreSection.setAttribute("style", "display: none");
+//    listEl.setAttribute("style", "display: none");
+    scoreButtons.setAttribute("style", "display: none");
+    returnButton.setAttribute("style", "display: none");
+//    listEl.setAttribute("style", "display: none");
     Answers.setAttribute("style", "margin: auto; width: 50%; text-align: left; font-size: 20px");
-    // span1.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; text-align: left; color: black; background-color: #7e307e;");
-    // span2.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
-    // span3.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
-    // span4.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
+    span1.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; text-align: left; color: black; background-color: #7e307e;");
+    span2.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
+    span3.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
+    span4.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
+    currentQuestion = 0;
     displayQuestion();
 });
 
@@ -310,9 +339,37 @@ function highScores (){
         localStorage.setItem("highScores", JSON.stringify(highScoreAr));
     }    
     console.log(currentUserScore);
+    displayScore();
 }
 
 initialButton.onclick = highScores;
+
+function stopTimer() {
+    var timeInterval = setInterval(function(){
+        if (timePause> 1) {
+            // if timer greater than 0 reduce timer
+            timePause --;
+        } else {
+            // time passed reset timer and go to next question
+            clearInterval(timeInterval)
+            console.log("stop timer");
+            if(currentQuestion == questions.length-1){
+                timePause = 2;
+                // showTime.setAttribute("style", "display: none");
+                // quizOverFlag = true;
+                 page = 2;
+                 timeLeft = 0;
+                displayResults()
+            } else {
+                currentQuestion++;
+            }
+            page = 1;
+            displayQuestion();
+        }
+    },1000);
+}
+
+
 
 function runningTimer() {
     var timeInterval = setInterval(function(){
@@ -322,55 +379,11 @@ function runningTimer() {
             message = "Time ";
             message = message.concat(timeLeft);
             showTime.textContent = message;
-            if (correctFlag) {
-                correctFlag = false
-                if(currentQuestion == questions.length-1){
-                    showTime.setAttribute("style", "display: none");
-                    quizOverFlag = true;
-                } else {
-                    currentQuestion++;
-                    displayQuestion();
-                }
-            } else if (wrongFlag) {
-                wrongFlag = false;
-                if(currentQuestion == questions.length-1){
-                    showTime.setAttribute("style", "display: none");
-                    quizOverFlag = true;
-                } else {
-                    currentQuestion++;
-                    displayQuestion();
-                }
-            }
         } else {
-            // time passed reset timer and go to next question
+            // time passed reset timer and show results
             clearInterval(timeInterval)
-            if(currentQuestion == questions.length-1){
-                // currentQuestion = 0;
-                quizOverFlag = true;
-                clearInterval(timeInterval);
-            } else {
-                currentQuestion++;
-            }
-
-            if (!quizOverFlag) {
-                // once answered do not let select another answer
-                showTime.setAttribute("style", "display: none");
-                answeredFlag = false;
-                Answers.setAttribute("style", "margin: auto; width: 50%; text-align: left; font-size: 20px");
-                // span1.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
-                // span2.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
-                // span3.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
-                // span4.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
-                displayQuestion();
-            } else {
-                horzLine.setAttribute("style", "display: none;");
-                result.setAttribute("style", "display: none;");
-                addInitial.setAttribute("style", "margin: auto; width: 50%; text-align: center; font-size: 20px");
-                initialButton.setAttribute("style", "margin-left: 1em;  padding: .5em; width: 20%; border-radius: .3em; background-color: #7e307e;");  
-
-                page = 2;
-                displayResults();
-            }
+            page =2;
+            displayResults();
         }
     },1000);
 }
@@ -381,6 +394,19 @@ function displayQuestion() {
     message = "Time ";
     message = message.concat(timeLeft);
     showTime.textContent = message;
+    startMessage.setAttribute("style", "display: none;");
+//   var listEl = document.createElement("ol");
+    Answers.appendChild(listEl);
+    listEl.appendChild(li1);
+    listEl.appendChild(li2);
+    listEl.appendChild(li3);
+    listEl.appendChild(li4);
+
+    li1.appendChild(span1);
+    li2.appendChild(span2);
+    li3.appendChild(span3);
+    li4.appendChild(span4);
+//    scoreSection.setAttribute("style", "display: none;");
     Answers.setAttribute("style", "margin: auto; width: 50%; text-align: left; font-size: 20px");
     span1.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
     span2.setAttribute("style", "margin: .5em; padding: .5em; border-radius: .3em; background-color: #7e307e;");
@@ -390,17 +416,24 @@ function displayQuestion() {
     spanStart.setAttribute("style", "display: none;");
 
 
-    h1El.textContent = questions[currentQuestion].question;
+    h1El.textContent = questions[currentQuestion].question;   
     span1.textContent = questions[currentQuestion].choices[0];
     span2.textContent = questions[currentQuestion].choices[1];
     span3.textContent = questions[currentQuestion].choices[2];
     span4.textContent = questions[currentQuestion].choices[3];
+
     horzLine.setAttribute("style", "display: none;");
     result.setAttribute("style", "display: none;");
+//    initialInput.textContent = "";
     runningTimer();
 }
 
 function displayResults() {
+    horzLine.setAttribute("style", "display: none;");
+    result.setAttribute("style", "display: none;");
+    addInitial.setAttribute("style", "margin: auto; width: 50%; text-align: center; font-size: 20px");
+    initialButton.setAttribute("style", "margin-left: 1em;  padding: .5em; width: 20%; border-radius: .3em; background-color: #7e307e;");  
+
     message = "All done!"
     h1El.textContent = message;
     message = "Your final score is:";
@@ -424,8 +457,13 @@ function displayStart(){
     span2.setAttribute("style", "display: none;");
     span3.setAttribute("style", "display: none;");
     span4.setAttribute("style", "display: none;");
-    h1El.setAttribute("style", "margin: auto; margin-top: 10%; width: 50%; text-align: center;");
 
+    h1El.setAttribute("style", "margin: auto; margin-top: 10%; width: 50%; text-align: center;");
+    startMessage.setAttribute("style", "margin: auto; width: 50%; text-align: center; font-size: 20px");
+    startButton.setAttribute("style", "margin-top: 3em; line-height: 1.2em;");
+    spanStart.setAttribute("class", "button");
+    spanStart.setAttribute("style", "margin-left: 48%;  padding: .5em; width: 20%; color: white; text-align: center; border-radius: .3em; background-color: #7e307e;");
+    
     displayNavBar();
     message = "Coding Quiz Challange"
     h1El.textContent = message;
@@ -438,16 +476,62 @@ function displayStart(){
 }
 
 function displayScore() {
-    message = "High Scores";
-    Answers.textContent = message;
+//    Answers.setAttribute("style", "margin: auto; width: 50%; text-align: left; font-size: 20px");
+
     var highScoreAr = JSON.parse(localStorage.getItem("highScores")) || [];
 
     console.log(highScoreAr.length);
     console.log(highScoreAr);
+    scoreListLength = highScoreAr.length;
     for (var i = 0; i < highScoreAr.length; i++) {
         console.log(highScoreAr[i].initials,highScoreAr[i].score);
     }
+
+    page = 3;
+    //Answers.setAttribute("style", "display: none;");
+    Answers.textContent = "";
+    startButton.setAttribute("style", "display: none");
+    //Answers.setAttribute("style", "display: none");
+
+    addInitial.setAttribute("style", "display: none;");
+    h1El.textContent = "High Scores";
+    var scorelistEl = document.createElement("ol");
+//    Answers.appendChild(listEl);
+    scoreSection.appendChild(scorelistEl);
+    scoreSection.setAttribute("style", "margin: auto; width: 50%; text-align: left; font-size: 20px");
+
+    for (var i = 0; i < highScoreAr.length; i++) {
+        var score = highScoreAr[i];
+        var li = document.createElement("li");
+        li.textContent =  score.initials + " - " + score.score;
+        li.setAttribute("data-index", i);
+        scorelistEl.appendChild(li);
+    }
+    scoreButtons.setAttribute("style", "margin-top: 3em; line-height: 1.2em;");
+    returnButton.setAttribute("style", "margin-left: 45%;  padding: .5em; width: 20%; color: white; text-align: center; border-radius: .3em; background-color: #7e307e;");
+    clearButton.setAttribute("style", "margin-left: 1em;  padding: .5em; width: 20%; color: white; text-align: center; border-radius: .3em; background-color: #7e307e;");    
+    returnButton.textContent = "Return";
+    clearButton.textContent = "Clear";
 }
+
+//clearButton.onclick = localStorage.clear();
+clearButton.addEventListener("click", event => {
+    localStorage.clear();
+//    scoreSection.setAttribute("style", "display: none;");
+    displayScore();
+});
+
+returnButton.addEventListener("click", event => {
+    Answers.setAttribute("style", "display: none");
+    scoreButtons.setAttribute("style", "display: none");
+    scoreSection.setAttribute("style", "display: none");
+    returnButton.setAttribute("style", "display: none");
+    page = 0;
+    correctAnswers = 0;
+    currentQuestion = 0;
+    timeLeft = 40;
+    displayStart();
+});
 
 showScore.onclick = displayScore;
 
@@ -458,7 +542,7 @@ if (page === 0) {
 } else if (page == 2) {
     displayResults();
 } else if (page == 3) {
-    displayHighScore();
+    displayScore();
 } else {
     displayResults();
 }
